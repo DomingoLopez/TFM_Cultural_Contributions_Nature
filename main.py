1a61e03be1bec03b87c40113fdbacd0d7e4e3f87
Domingo López <domin68914@gmail.com>
2024-10-23 18:41:38 +0200

    Funcionando

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from src.image_loader import ImageLoader
-from src.dinov2_inference import Dinov2Inference
+import pandas as pd
+
+from src.utils.image_loader import ImageLoader
+from src.dinov2_inference.dinov2_inference import Dinov2Inference
+
 
 
 if __name__ == "__main__":
     # Finding images
-    image_loader = ImageLoader(folder="./data/test/")
+    image_loader = ImageLoader(folder="./data/Data")
     images = image_loader.find_images()
     # Loading images and getting embeddings
     dinomodel = Dinov2Inference(model_name="small", images=images)
     embeddings = dinomodel.run()
-    print(len(embeddings))
     # TODO: Load embeddings into df. 
     # TODO: Dimensionality reduction (or not)
     # TODO: Apply Clustering techniques
